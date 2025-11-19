87de4c978727d75537f05b190b17cc06283844a3
girlcalledblu <45247880+girlcalledblu@users.noreply.github.com>
2025-11-18 19:44:03 -0800

    Update juryrig_action.rb

--- plugins/fs3combat/actions/juryrig_action.rb
+++ plugins/fs3combat/actions/juryrig_action.rb
@@ -60,7 +60,8 @@ module AresMUSH
                     if (!self.combatant.is_npc?)
                   
                 Achievements.award_achievement(self.combatant.associated_model, "fs3_juryrigged")
-
+                    end
+                
                 FS3Combat.heal(wound, 1)
                     return [  "#{tech_model.name} successfully repaired #{pilot_model.name} worst damage." ]
                end
