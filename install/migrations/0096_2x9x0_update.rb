18302b1be16f84e0078e4fbed76dbaa7116dd588
lynnfaraday <lynnfaraday@users.noreply.github.com>
2025-12-27 18:54:17 -0500

    Chargen intro blurb.

--- install/migrations/0096_2x9x0_update.rb
+++ install/migrations/0096_2x9x0_update.rb
@@ -13,6 +13,15 @@ module AresMUSH
         config['achievements']['notification_category'] = "System Notices"
         config['achievements']['notification_days'] = 1
         DatabaseMigrator.write_config_file("achievements.yml", config)
+        
+        
+        Global.logger.debug "Adding chargen intro blurb."
+        config = DatabaseMigrator.read_config_file("chargen.yml")
+        config['chargen']['intro_blurb'] = "The online character creation tool will guide you through creating your character."
+        DatabaseMigrator.write_config_file("chargen.yml", config)
+              
+
+        
       end
     end
   end    
