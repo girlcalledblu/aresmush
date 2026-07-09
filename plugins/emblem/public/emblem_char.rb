8e25a26f1209651dcda9c5efe2a8b79804bbcd77
girlcalledblu <45247880+girlcalledblu@users.noreply.github.com>
2026-07-09 13:29:28 -0700

    yml update

--- plugins/emblem/public/emblem_char.rb
+++ plugins/emblem/public/emblem_char.rb
@@ -2,5 +2,17 @@ module AresMUSH
   class Character
     attribute :emblem
 
+
+    def self.derive_emblem(char)
+      if Global.read_config('emblem', 'field_type') == "Demographic"
+
+      elsif Global.read_config('emblem', 'field_type') == "Group"
+      
+      elsif Global.read_config('emblem', 'field_type') == "Custom"
+
+      else
+
+      end
+    end
   end  
 end
