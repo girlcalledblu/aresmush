3055312d7e73687727651d30e881dbf66cdbf779
girlcalledblu <45247880+girlcalledblu@users.noreply.github.com>
2026-07-09 13:59:25 -0700

    Update custom_char_fields.rb

--- plugins/profile/custom_char_fields.rb
+++ plugins/profile/custom_char_fields.rb
@@ -14,7 +14,7 @@ module AresMUSH
       #    return { goals: Website.format_markdown_for_html(char.goals) }
       def self.get_fields_for_viewing(char, viewer)
         return {
-          emblem: Emblem.derive_emblem(char)
+          emblem: Character.derive_emblem(char)
         }
       end
     
