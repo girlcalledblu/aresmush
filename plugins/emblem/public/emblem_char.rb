8c59075d7a05fbdfee2db640303ee57aff59c472
girlcalledblu <45247880+girlcalledblu@users.noreply.github.com>
2026-07-09 13:48:36 -0700

    Update emblem_char.rb

--- plugins/emblem/public/emblem_char.rb
+++ plugins/emblem/public/emblem_char.rb
@@ -5,11 +5,18 @@ module AresMUSH
 
     def self.derive_emblem(char)
       if Global.read_config('emblem', 'field_type') == "Demographic"
-
+        field = Global.read_config('emblem', 'field')
+        demographic = char.demographics[field]
+        emblem = demographic
+        return emblem
       elsif Global.read_config('emblem', 'field_type') == "Group"
-      
+        field = Global.read_config('emblem', 'field')
+        group = char.groups[field]
+        emblem = group
+        return emblem
       elsif Global.read_config('emblem', 'field_type') == "Custom"
-
+        emblem = char.emblem
+        return emblem
       else
 
       end
